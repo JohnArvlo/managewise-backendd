f4777c111c82fd085effc8c79643d40eedf070e7
John_Arvlo <u202117377@upc.edu.pe>
2024-11-19 01:43:56 -0500

    fix: members bounded context.

--- src/main/java/pe/edu/upc/managewise/backend/backlog/domain/model/valueobjects/TaskList.java
+++ src/main/java/pe/edu/upc/managewise/backend/backlog/domain/model/valueobjects/TaskList.java
@@ -1,8 +1,6 @@
 package pe.edu.upc.managewise.backend.backlog.domain.model.valueobjects;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import pe.edu.upc.managewise.backend.backlog.domain.model.aggregates.UserStory;
 import pe.edu.upc.managewise.backend.backlog.domain.model.entities.TaskItem;
--- src/main/java/pe/edu/upc/managewise/backend/backlog/interfaces/rest/transform/UpdateUserStoryCommandFromResourceAssembler.java
+++ src/main/java/pe/edu/upc/managewise/backend/backlog/interfaces/rest/transform/UpdateUserStoryCommandFromResourceAssembler.java
@@ -2,6 +2,7 @@ package pe.edu.upc.managewise.backend.backlog.interfaces.rest.transform;
 
 import pe.edu.upc.managewise.backend.backlog.domain.model.commands.UpdateUserStoryCommand;
 import pe.edu.upc.managewise.backend.backlog.interfaces.rest.resources.UpdateUserStoryResource;
+import pe.edu.upc.managewise.backend.backlog.interfaces.rest.resources.UserStoryResource;
 
 public class UpdateUserStoryCommandFromResourceAssembler {
     //public static UpdateUserStoryCommand toCommandFromResource(Long id, UserStoryResource resource){
